2c23e82d737d45c998a9ea36d3fbeda38cd7b598
arch <enrsalut@gmail.com>
2026-09-24 00:54:56 +0200

    bug

--- client.py
+++ client.py
@@ -8,23 +8,37 @@ async def ainput(prompt: str = "") -> str:
 async def mk_json(visibility: str = "", msg: str = "") -> str:
     return dumps({"visibility" : visibility, "content" : f"{msg} + visibility : {visibility}"})+ "\n"
 
-async def main():
-    reader, writer = await asyncio.open_connection("127.0.0.1", 8888)
-    print("Connecté au serveur")
-
+async def server_response(reader):
     while True:
+        
+        reponse = await reader.readline()
+        print(f"Réponse : {reponse.decode()!r}")
+
+async def send_msg(writer):
+    while True:
+
         msg = await ainput("Ton nom : ")
 
         if msg == "[close]":
-            break
+            raise SystemError
 
         result = await mk_json(choice(["private", "global"]), msg)
         writer.write(result.encode())
         await writer.drain()
 
-        reponse = await reader.readline()
-        print(f"Réponse : {reponse.decode()!r}")
 
+async def main():
+    reader, writer = await asyncio.open_connection("127.0.0.1", 8888)
+    print("Connecté au serveur")
+
+
+    async with asyncio.TaskGroup() as tg:
+        tg.create_task(server_response(reader))
+        tg.create_task(send_msg(writer))
+
+        except
+
+    
     writer.close()
     await writer.wait_closed()
             
